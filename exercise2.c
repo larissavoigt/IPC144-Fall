a65f0ca5feda518c2e7e2e6c21e80b870bc7d33c
Larissa Voigt <larissavoigt@gmail.com>
2015-01-23 20:29:13 -0500

    Delete exercise2.c

--- exercise2.c
+++
@@ -1,4 +0,0 @@
-/* Date: Oct 20th 2014
- * Purpose: Program that asks the user to enter a 4 digit number in the range 1000-9999.
- * Displays each digit of the number in a single line, ant tells if its even or odd.
- */
